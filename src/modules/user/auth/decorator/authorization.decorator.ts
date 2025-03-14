0b3dc33059187364697341a77dfc8346e26c4f48
junyeob kim <junyeob.kim@onch3.co.kr>
2025-03-14 15:25:58 +0900

    feat: codecleanup

--- src/modules/user/auth/decorator/authorization.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-
-export const Authorization = createParamDecorator(
-    (data: any, context: ExecutionContext) => {
-        const req = context.switchToHttp().getRequest();
-
-        return req.headers['authorization'];
-    }
-)
