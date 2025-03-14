749177811a23c55ebeb2db9b69d5f335d5d1c0ae
junyeob kim <junyeob.kim@onch3.co.kr>
2025-03-14 17:49:30 +0900

    feat: unittest 셋팅중..

--- src/modules/user/auth/auth.controller.spec.ts
+++ src/modules/user/auth/auth.controller.spec.ts
@@ -1,20 +1,21 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { TestBed } from '@automock/jest';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { QueryRunner } from 'typeorm';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service   : jest.Mocked<AuthService>;
+  let qr        : jest.Mocked<QueryRunner>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [AuthService],
-    }).compile();
-
-    controller = module.get<AuthController>(AuthController);
+    const {unit, unitRef} = TestBed.create(AuthController).compile();
+    controller            = unit;
+    service               = unitRef.get<AuthService>(AuthService);
+    qr                    = {} as any as jest.Mocked<QueryRunner>;
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
--- src/modules/user/auth/auth.service.spec.ts
+++ src/modules/user/auth/auth.service.spec.ts
@@ -1,15 +1,30 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { Product } from '../product/entities/product.entity';
+import { UserProduct } from './entities/user-product.entity';
+import { Order } from '../order/entities/order.entity';
+import { OrderProduct } from '../order/entities/order-product.entity';
+import { TestBed } from '@automock/jest';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let service               : AuthService;
+  let userRepository        : jest.Mocked<Repository<User>>;
+  let productRepository     : jest.Mocked<Repository<Product>>;
+  let userProductRepository : jest.Mocked<Repository<UserProduct>>;
+  let orderRepository       : jest.Mocked<Repository<Order>>;
+  let orderProductRepository: jest.Mocked<Repository<OrderProduct>>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+    const {unit, unitRef} = TestBed.create(AuthService).compile();
 
-    service = module.get<AuthService>(AuthService);
+    service                 = unit;
+    userRepository          = unitRef.get(getRepositoryToken(User) as string);
+    productRepository       = unitRef.get(getRepositoryToken(Product) as string);
+    userProductRepository   = unitRef.get(getRepositoryToken(UserProduct) as string);
+    orderRepository         = unitRef.get(getRepositoryToken(Order) as string);
+    orderProductRepository  = unitRef.get(getRepositoryToken(OrderProduct) as string);
   });
 
   it('should be defined', () => {
